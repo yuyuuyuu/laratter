a6bc14fcda324861ae4b4051340a6a4ef753b7a5
yuyuuyuu <fujiyoshi.yusuke.671@s.kyushu-u.ac.jp>
2023-09-05 20:14:44 +0900

    tweet作成実装

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\Tweet;
+
+class TweetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $tweets = Tweet::getAllOrderByUpdated_at();
+        return response()->view('tweet.index',compact('tweets'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return response()->view('tweet.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // バリデーション
+        $validator = Validator::make($request->all(), [
+          'tweet' => 'required | max:191',
+         'description' => 'required',
+        ]);
+        // バリデーション:エラー
+        if ($validator->fails()) {
+          return redirect()
+            ->route('tweet.create')
+            ->withInput()
+            ->withErrors($validator);
+        }
+        // create()は最初から用意されている関数
+        // 戻り値は挿入されたレコードの情報
+        $result = Tweet::create($request->all());
+        // ルーティング「todo.index」にリクエスト送信（一覧ページに移動）
+        return redirect()->route('tweet.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
